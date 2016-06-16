8f9d0975bd82132f976064bab15c743ffa2ab651
Wally <wally@quadrantalpha.com>
2016-06-16 11:22:27 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+	render (){
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+	 return (<div>
+				Hello World
+			</div>
+		);
+	}	
+}
+
+
+ReactDOM.render(<App />,document.querySelector('.container'));
